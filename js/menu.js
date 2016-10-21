ada3423446a563a38735d9d816f30340f6cf7374
defunSM <sxh8083@g.rit.edu>
2016-10-20 21:15:10 -0400

    testing

--- js/menu.js
+++ js/menu.js
@@ -11,7 +11,7 @@ $('.menu').click(function() {
   });
 
 $(window).scroll(function(){
-    if  ($(window).scrollTop() >= 1750){
+    if  ($(window).scrollTop() >= 1790){
       	$(".fadeInBlock").fadeIn(100);
     }
     else{
