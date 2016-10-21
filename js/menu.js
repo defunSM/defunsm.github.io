05d7e9842b82492e302f51119329766247553f5c
defunSM <sxh8083@g.rit.edu>
2016-10-20 21:44:45 -0400

    testing

--- js/menu.js
+++ js/menu.js
@@ -12,10 +12,10 @@ $('.menu').click(function() {
 
 $(window).scroll(function(){
     if  ($(window).scrollTop() >= 1900){
-        $('#fadeInBlock').show("slide", { direction: "right" }, 1000);
+        $('#fadeInBlock').show();
 
     }
     else{
-        $('.fadeInBlock').hide("slide", {direction: "left" }, 1000);
+        $('.fadeInBlock').hide();
     }
 });
