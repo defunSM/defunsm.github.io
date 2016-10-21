9eb0a0058c99ea95e53c01f4afa1df4349ce8841
defunSM <sxh8083@g.rit.edu>
2016-10-20 20:40:44 -0400

    testing

--- js/menu.js
+++ js/menu.js
@@ -24,11 +24,11 @@ $(function() {
 
     if( bottom_of_window > bottom_of_object ){
 
-        $(this).animate({'opacity':'1'},50);
+        $(this).animate({'opacity':'1'},200);
 
             }
             else{
-              $(this).animate({'opacity':'0'},50);
+              $(this).animate({'opacity':'0'},200);
             }
         });
 
