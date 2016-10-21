df56dc891ebbeed688a2ac0da12b9a1b36b2bbd3
defunSM <sxh8083@g.rit.edu>
2016-10-20 21:16:19 -0400

    testing

--- js/menu.js
+++ js/menu.js
@@ -11,7 +11,7 @@ $('.menu').click(function() {
   });
 
 $(window).scroll(function(){
-    if  ($(window).scrollTop() >= 1790){
+    if  ($(window).scrollTop() >= 1850){
       	$(".fadeInBlock").fadeIn(100);
     }
     else{
