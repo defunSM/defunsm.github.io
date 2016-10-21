3f4e539670c114204d2ad715e340a5fe9180ec5e
defunSM <sxh8083@g.rit.edu>
2016-10-20 20:32:29 -0400

    fixed top icon missing in tags

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,28 @@ $('.menu').click(function() {
    $( "#toggle" ).removeClass( "active" )
    $('#navigation').removeClass('open');
   });
+
+$(function() {
+  $(window).scroll( function(){
+
+
+   $('.fadeInBlock').each( function(i){
+
+    var bottom_of_object = $(this).position().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+    bottom_of_window = bottom_of_window + 350;
+
+    if( bottom_of_window > bottom_of_object ){
+
+        $(this).animate({'opacity':'1'},500);
+
+            }
+            else{
+              $(this).animate({'opacity':'0'},500);
+            }
+        });
+
+    });
+});
