20e306df70f6e5a6dce4bfee23709a6531df15c6
defunsm <sxh8083@g.rit.edu>
2017-01-07 22:11:03 -0500

    new updates

--- js/menu.js
+++ js/menu.js
@@ -22,7 +22,7 @@ $('.menu').click(function() {
 
 $(window).scroll(function() {
 
-    if ($(this).scrollTop()>550)
+    if ($(this).scrollTop()>650)
      {
         $('.burger').fadeOut();
      }
@@ -34,7 +34,7 @@ $(window).scroll(function() {
 
 $(window).scroll(function() {
 
-    if ($(this).scrollTop()<550)
+    if ($(this).scrollTop()<650)
      {
         $('ul3').fadeOut();
      }
