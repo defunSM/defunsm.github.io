393c0d466ab5260e777a75187d8374e685dd1700
defunsm <sxh8083@g.rit.edu>
2017-01-13 18:29:02 -0500

    some changes to color scheme

--- js/menu.js
+++ js/menu.js
@@ -78,6 +78,6 @@ $('code').on('click', function() {
 
 
 $("i").hover(
-    function(e){ $(this).animate({ marginTop: -100 }); }, // over
-    function(e){ $(this).animate({ marginTop: 100 }); }  // out
+    function(e){ $(this).animate({ marginTop: -10 }); }, // over
+    function(e){ $(this).animate({ marginTop: 0 }); }  // out
 );
