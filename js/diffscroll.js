8891cce605f7b662d6183c8ebde5281a9b05aa6c
defunsm <sxh8083@g.rit.edu>
2017-01-13 22:01:35 -0500

    some changes to color scheme

---
+++ js/diffscroll.js
@@ -0,0 +1,79 @@
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('.body').toggleClass('body_hidden')
+   $('#navigation').toggleClass('open');
+  });
+
+$('.menu').click(function() {
+   $('.body').removeClass('body_hidden')
+   $( "#toggle" ).removeClass( "active" )
+   $('#navigation').removeClass('open');
+  });
+
+// $(window).scroll(function(){
+//     if  ($(window).scrollTop() >= 300){
+//         $(".burger").hide();
+
+//     }
+//     else{
+//         $(".burger").show();
+//     }
+// });
+
+$(window).scroll(function() {
+
+    var wScroll = $(this).scrollTop();
+
+    if ($(this).scrollTop()>350)
+     {
+        $('.burger').fadeOut();
+     }
+    else
+     {
+      $('.burger').fadeIn();
+     }
+
+        if ($(this).scrollTop()<350)
+     {
+         $('ul3').fadeOut();
+         $('li3').fadeOut();
+         $('.menubarblack').fadeOut();
+
+     }
+    else
+     {
+         $('ul3').fadeIn();
+         $('li3').fadeIn();
+         $('.menubarblack').fadeIn();
+         // $('.menubarblack').css({'height':  + 'px'});
+
+     }
+
+
+
+    $('.header_title').css({
+        'transform' : 'translate(0px, ' + wScroll/3 + 'px)'
+    });
+
+    $('.header_subtitle').css({
+        'transform' : 'translate(0px, ' + wScroll/3 + 'px)'
+    });
+
+});
+
+$('code').on('click', function() {
+
+    var myvar = $(this).css('font-size')
+
+    if (myvar == "30px") {
+
+        $(this).css({'font-size':'inherit'});
+    }
+    else {
+
+        $(this).css({'font-size':'30px'});
+
+    }
+
+
+});
